834e4448630c0e0d2f2ca52e71887116ae527c16
Diogo Medeiros <diogo_medeiros@sapo.pt>
2023-04-10 11:51:22 +0100

    fixed memory issues by converting image to Gray8 format before encrypting

--- CryptoTools/CryptoTools/Utils/BitmapUtils.cs
+++ CryptoTools/CryptoTools/Utils/BitmapUtils.cs
@@ -1,44 +1,12 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Windows.Media.Imaging;
 
 namespace CryptoTools.Utils;
 
 public static class BitmapUtils
 {
-    public static Bitmap Grayscale(Bitmap input)
-    {
-        var output = new Bitmap(input.Width, input.Height, PixelFormat.Format8bppIndexed);
-        var grayscale = output.Palette;
-        for (var i = 0; i < grayscale.Entries.Length; i++) grayscale.Entries[i] = Color.FromArgb(i, i, i);
-        var rect = new Rectangle(0, 0, input.Width, input.Height);
-        var bmpData = input.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-        var pixelData = new byte[bmpData.Stride * input.Height];
-        Marshal.Copy(bmpData.Scan0, pixelData, 0, pixelData.Length);
-        input.UnlockBits(bmpData);
-        bmpData = output.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
-        var rowBytes = new byte[bmpData.Stride];
-        for (var y = 0; y < bmpData.Height; y++)
-        {
-            var offset = y * bmpData.Stride;
-            for (var x = 0; x < bmpData.Width; x++)
-            {
-                var pixelOffset = offset + x;
-                var r = pixelData[pixelOffset * 3];
-                var g = pixelData[pixelOffset * 3 + 1];
-                var b = pixelData[pixelOffset * 3 + 2];
-                rowBytes[x] = (byte)(r * 0.299 + g * 0.587 + b * 0.114);
-            }
-
-            Marshal.Copy(rowBytes, 0, bmpData.Scan0 + offset, rowBytes.Length);
-        }
-
-        output.UnlockBits(bmpData);
-        return output;
-    }
-
     public static BitmapImage ToBitmapImage(string fileName)
     {
         using var stream = new FileStream(fileName, FileMode.Open);
--- CryptoTools/CryptoTools/ViewModels/ImageEncryptionViewModel.cs
+++ CryptoTools/CryptoTools/ViewModels/ImageEncryptionViewModel.cs
@@ -2,8 +2,11 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using CryptoLib.Models;
 using CryptoTools.Utils;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
 namespace CryptoTools.ViewModels;
 
@@ -23,24 +26,22 @@ public class ImageEncryptionViewModel : ViewModelBase
         algorithm.GenerateKey();
         algorithm.GenerateIV();
         // Load the image and extract the pixel data
-        using var bitmap = new Bitmap(imagePath);
+        var bitmapImage = BitmapUtils.ToBitmapImage(imagePath);
         // Convert the image to grayscale
-        // This helps to reduce the size of the encrypted image, which is useful for large images
-        using var grayscaleBitmap = BitmapUtils.Grayscale(bitmap);
+        var grayscale = new FormatConvertedBitmap(bitmapImage, PixelFormats.Gray8, null, 0);
         // Extract the pixel data from the grayscale image
-        var rect = new Rectangle(0, 0, grayscaleBitmap.Width, grayscaleBitmap.Height);
-        var bmpData = grayscaleBitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format8bppIndexed);
-        var numBytes = bmpData.Stride * grayscaleBitmap.Height;
+        var stride = grayscale.PixelWidth;
+        var numBytes = stride * grayscale.PixelHeight;
         var pixelData = new byte[numBytes];
-        Marshal.Copy(bmpData.Scan0, pixelData, 0, numBytes);
-        grayscaleBitmap.UnlockBits(bmpData);
+        grayscale.CopyPixels(pixelData, stride, 0);
         // Encrypt the pixel data
         using var encryptor = algorithm.CreateEncryptor();
-        var encryptedPixelData = encryptor.TransformFinalBlock(pixelData, 0, numBytes);
+        encryptor.TransformBlock(pixelData, 0, pixelData.Length, pixelData, 0);
         // Save the encrypted pixel data as a new image
-        var encryptedBitmap = new Bitmap(grayscaleBitmap.Width, grayscaleBitmap.Height, PixelFormat.Format8bppIndexed);
+        var encryptedBitmap = new Bitmap(grayscale.PixelWidth, grayscale.PixelHeight, PixelFormat.Format8bppIndexed);
+        var rect = new Rectangle(0, 0, grayscale.PixelWidth, grayscale.PixelHeight);
         var encryptedBmpData = encryptedBitmap.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
-        Marshal.Copy(encryptedPixelData, 0, encryptedBmpData.Scan0, numBytes);
+        Marshal.Copy(pixelData[..numBytes], 0, encryptedBmpData.Scan0, pixelData.Length);
         encryptedBitmap.UnlockBits(encryptedBmpData);
         return encryptedBitmap;
     }
