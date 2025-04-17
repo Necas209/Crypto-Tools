58131321ce688d5d41b207bc2b3dab3cdfadd5b1
Diogo Medeiros <diogo_medeiros@sapo.pt>
2025-04-17 14:50:01 +0100

    Removed `AddControllers` call from Program.cs

--- CryptoServer/Program.cs
+++ CryptoServer/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<CryptoDbContext>(
     options => options.UseSqlServer(
         Environment.GetEnvironmentVariable("SQLCONNSTR_CryptoDb")
